83b09350c74aec8a95c2c8f121ae47c99d079ab0
rickytan <ricky.tan.xin@gmail.com>
2015-07-11 10:07:53 +0800

    async

--- js/welcome.js
+++ js/welcome.js
@@ -183,5 +183,13 @@
         else
             zoomToPosition(initial_position, initial_zoom);
     }
-
+// Google Analytics: change UA-XXXXX-X to be your site's ID.
+    setTimeout(function() {
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-41902529-2', 'auto');
+ga('send', 'pageview');
+    }, 1000);
 })();
